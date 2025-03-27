a092fb345ac1c77afbcbc359e46fdc34faecec50
Saurabh kumar <saurabhhh2613@gmail.com>
2025-03-27 11:43:20 +0530

    make your offer form redesigned

--- src/Component/Post/SinglePostDetails/BiddingForm.jsx
+++ src/Component/Post/SinglePostDetails/BiddingForm.jsx
@@ -194,7 +194,7 @@ export default function BiddingFormForm({
     // Update the input value to display with commas
     event.target.value = formattedPrice;
   };
-
+console.log(PropertyAddress)
   return (
     <>
       {/* <div className="bidding-form-container"> */}
@@ -206,25 +206,34 @@ export default function BiddingFormForm({
           onSubmit={BiddingFormSubmitHandler}
           encType="multipart/form-data"
         >
-          <div className="cross-div">
-            <span
-              className="cross-btn"
-              onClick={() => {
-                SetShow(false);
-              }}
-            >
-              X
-            </span>
-          </div>
-          <h2 className="mke-you-offer-title">Make your Offer</h2>
-          <div className="main-box-content">
-            <div className="mke-you-offer-prperty-address-box">
-              <div className="apart-prop-id-box">
-                <p className="form-heading-name">{PropertyAddress}</p>
-              </div>
-            </div>
+          <div className="make-your-offer-top">
+          
 
-            <div className="make-offer-reserved-price">
+          <div>
+          <h2 className="mke-you-offer-title">Make your Offer</h2>
+          </div>
+          <div   className="make-your-offer-crozz-btn">
+          <img
+          onClick={() => {
+            SetShow(false);
+          }}
+              className=" make-your-offer-cross-btn"
+              src="/img/mke-offer-cross.svg"
+             
+            />
+            
+          </div>
+            
+          
+          
+          </div>
+         
+         
+          <div className="main-box-content">
+
+
+           
+          <div className="make-offer-reserved-price">
               <div className="reserve-price-section-form">
                 <div>
                   <p className="price-que-form">
@@ -234,22 +243,47 @@ export default function BiddingFormForm({
                       {formatReservePrice(
                         SinglePostData.SinglePost.PricingDetails.ExpectedPrice
                       )}{" "}
-                    </span>{" "}
+                    </span><span className="area-section-from pt-1">{" ₹"}{SinglePostData.SinglePost.PricingDetails.PricePerSqFt}
+                    /sqft{" "}</span>
                   </p>
 
-                  <p className="area-section-from pt-1">
+                  {/* <p className="area-section-from pt-1">
                     {SinglePostData.SinglePost.PricingDetails.PricePerSqFt} per
                     sqft
-                  </p>
+                  </p> */}
                 </div>
                 <div className="make-offer-property-id space-y-1">
-                  <p className="prop-id-from">Property Id </p>
+                  <p className="prop-id-from">Property Id:  <span>{SinglePostData.SinglePost._id}</span> </p>
                   <p className="prop-id-number ">
-                    {SinglePostData.SinglePost._id}
+                    
                   </p>
                 </div>
               </div>
             </div>
+
+            <div className="make-offer-data-container">
+
+              <div className="make-offer-table-container">
+
+              <div className="mke-you-offer-prperty-address-box">
+              <div className="apart-prop-id-box">
+                <p className="form-heading-name">{PropertyAddress}</p>
+               
+                
+              </div>
+            </div>
+            <div className="priceNoffer">
+              <div className="highest-price-container"> 
+                <div className="highest-price-img-container"><img src="/img/highestprice.svg" alt="highest-price" /></div>
+                <div className="highest-price-text"><p>Highest Price recieved: <span>₹ 500 CR</span></p></div>
+              </div>
+              <div className="total-offer">
+              <div className="total-offer-img-container"><img src="/img/Totaloffer.svg" alt="total-offer"/></div>
+              <div className="total-price-text"><p>Total Offer: <span>100</span></p></div>
+              </div>
+            </div>
+
+            
 
             {/* <div className="mke-you-offer-bids-info">
             <p className="mke-you-offer-bids-text">
@@ -567,12 +601,20 @@ export default function BiddingFormForm({
                   }}
                   className="mke-you-offer-checkbox-input"
                 />
-                Receive updates on WhatsApp
+                Receive updates on WhatsApp <img src="/img/make-offer-whatsapp.svg" alt="whatsapp" />
               </label>
             </div>
+              </div>
+              <div className="make-offer-image-container">
+                <img src="/img/make-offer-image.svg" alt="img" />
+              </div>
+
+            
+            </div>
+            
 
             <button type="submit" className="mke-you-offer-submit-button">
-              Submit
+              Submit Offer
             </button>
           </div>
         </form>
